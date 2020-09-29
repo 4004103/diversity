775cf73e9d6faa0bd2e82ba2a3b599a3ee47b76a
SU A JO <>
2020-09-29 09:53:59 +0900

    한국다양성연구소

---
+++ js/pub.js
@@ -0,0 +1,61 @@
+$(function() {
+
+    /* nav */
+
+
+    //slide way
+    var $nav = $(".nav")
+    var $nav_list = $(".nav__item > a");
+
+    $nav_list.on("mouseover focus", function(e) {
+        $nav.addClass("on");
+    });
+    $nav.on("mouseleave", function(e) {
+        $nav.removeClass("on");
+    });
+
+    $(".nav__item-2dep:last > a").on("blur", function() {
+        $nav.removeClass("on");
+    });
+
+
+    /* 메인 배너 */
+    var bnSlider = $('#bnSlider').bxSlider({
+        //autoHover: true,
+        //pagerSelector: '.selector', //셀렉터에 pager를 $selector에 append
+        //autoControls: true, //재생 정지버튼
+        //autoControlsSelector: '.selector', //재생,정지버튼을 $selector에  append
+        mode:'fade',
+        //tickerHover: true,
+        //autoHover:true,
+        auto:true,
+        controls:true,
+        pause: 3000,
+		pager: true,
+		/* onSliderLoad: function(){
+			$(".main-banner").css("visibility", "visible").animate({opacity:1});
+		}, */
+        //autoControls: true, //재생,정지버튼
+        prevSelector: '.main-banner__btn--prev',
+        nextSelector: '.main-banner__btn--next'
+    });
+
+	/* 메인 notice 탭메뉴 */
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+		$('ul.tabs li').removeClass('current');
+		$('.tab_content').removeClass('current');
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	});
+
+	/* 상단으로 */
+	$("#top_btn").on("click", function() {
+		$("html, body").animate({scrollTop:0}, '500');
+		return false;
+	});
+
+	/* 제브라툴팁 */
+	new $.Zebra_Tooltips($('.tooltips'));
+
+});
